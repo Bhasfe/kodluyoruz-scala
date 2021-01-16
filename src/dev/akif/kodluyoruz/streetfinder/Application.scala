bb71d6d4f4b8af490d0f8403a8238c1ee7f56225
Barış Hasdemir <33374731+Bhasfe@users.noreply.github.com>
2021-01-16 14:17:58 +0200

    first solution

--- src/dev/akif/kodluyoruz/streetfinder/Application.scala
+++ src/dev/akif/kodluyoruz/streetfinder/Application.scala
@@ -1,5 +1,7 @@
 package dev.akif.kodluyoruz.streetfinder
 
+import java.io.File
+
 /**
  * See CSV file at: https://github.com/makiftutuncu/kodluyoruz-scala/blob/main/data/streets.csv
  *
@@ -8,5 +10,22 @@ package dev.akif.kodluyoruz.streetfinder
 object Application {
   def main(args: Array[String]): Unit = {
 
+    // Traits
+    val loader = new CsvLoader{}
+    val finder = new StreetFinder{}
+
+    // Read the file
+    val file = new File("D:/kodluyoruz-scala/data/streets.csv")
+    val data = loader.loadCsv(file)
+
+    // Search the keywords
+    val search = scala.io.StdIn.readLine().split(" ").toSet
+    val matches = finder.findStreets(data,search)
+
+    // Print the results
+    for(element<-matches)
+    {
+      println(element)
+    }
   }
 }
